ec80854a082df4eb9f545eaabe836c929886a14f
daparicio55 <daparicio@idexperujapon.edu.pe>
2023-12-08 12:35:18 -0500

    impresion de constancia

--- resources/views/sacademica/practicas/constancia.blade.php
+++ resources/views/sacademica/practicas/constancia.blade.php
@@ -4,36 +4,35 @@
     <title>Sistema IDEX Perú Japón</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.css') }}">
 </head>
 <body>
-    <div class="container">
-        <br>
+    <div class="container" style="padding-right: 15%; padding-left: 15%">
+        
         <div class="row justify-content-md-center mt-5">
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 mt-5">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
                 <div style="text-align: center">
                     <h4 class="mt-5"><b>EL DIRECTOR GENERAL DEL INSTITUTO DE EDUCACIÓN SUPERIOR TECNOLÓGICO PÚBLICO "PERÚ JAPÓN" DE CHACHAPOYAS</b></h4>
                 </div>
             </div>
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 mt-4">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-4">
                 <div style="text-align: left">
                     <h6>OTORGA LA PRESENTE:</h6>
                 </div>
             </div>
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 mt-5">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
                 <div style="text-align: center">
                     <b><h2>CONSTANCIA DE PRÁCTICA PRE PROFESIONAL</h2></b>
                 </div>
             </div>
             
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div style="text-align: center">
                     <b><h3>A: {{ Str::upper($estudiante->postulante->cliente->apellido) }}, {{ Str::upper($estudiante->postulante->cliente->nombre) }}</h3></b>
                 </div>
             </div>
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 mt-3">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-3">
                 <p style="font-size: 1.3rem" class="text-justify">
                     Del programa de estudios de <b style="text-decoration: underline" class="text-uppercase">{{ $estudiante->postulante->carrera->nombreCarrera }}</b>
                     quien ha cumplido con realizar y presentar su informe, fichas de evaluacion y demás documentos de sus prácticas pre profesional que corresponden a los siguientes módulos:
@@ -46,7 +45,7 @@
                 $nota = 0;
                 $promedio = 0;
             @endphp
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 @foreach ($estudiante->practicas as $practica)
                     <p style="font-size: 1.3rem">
                         <b>{{ $practica->modulo->nombre }}</b>
@@ -62,7 +61,7 @@
             @php
                 $promedio = round($suma / $contador,0);
             @endphp
-            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <p>
                     Obteniendo un promedio final de <strong> {{ Rmunate\Utilities\SpellNumber::integer($promedio)->toLetters() }} ({{ $promedio }})</strong> , 
                     con registro de fecha {{ __(Carbon\Carbon::parse($practica->fpresentacion)->format('l')) }} {{ date('j',strtotime($practica->fpresentacion)) }} de
